d93d015c53cda02c4d8dd9efbdf39577beeda336
sgr-src <sgrebenjuk@soreco.ch>
2014-10-06 11:25:54 +0200

    changed anchor description
    formated product class

--- src/main/java/soreco/training/designpatterns/creational/factory/Product.java
+++ src/main/java/soreco/training/designpatterns/creational/factory/Product.java
@@ -1,17 +1,16 @@
 package soreco.training.designpatterns.creational.factory;
 
-
 public abstract class Product {
 
     private String name;
 
-    public void setName (String name){
+    public void setName(String name) {
 
         this.name = name;
     }
 
-    public String getName (){
+    public String getName() {
 
         return "My name is " + name;
-}
+    }
 }
