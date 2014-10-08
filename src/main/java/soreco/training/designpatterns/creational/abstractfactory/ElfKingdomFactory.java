8e8c66baba57798b5e3fcfcce4ca68c304b8d4e2
SergejG <sergej.grebenjuk@gmail.com>
2014-10-08 13:24:16 +0200

    added - structure for abstract factory test

---
+++ src/main/java/soreco/training/designpatterns/creational/abstractfactory/ElfKingdomFactory.java
@@ -0,0 +1,7 @@
+package soreco.training.designpatterns.creational.abstractfactory;
+
+/**
+ * Created by Sergej Grebenjuk on 08.10.14.
+ */
+public class ElfKingdomFactory {
+}
---
+++ src/test/java/soreco/training/designpatterns/creational/abstractfactory/test/AbstractFactoryTest.java
@@ -0,0 +1,7 @@
+package soreco.training.designpatterns.creational.abstractfactory.test;
+
+/**
+ * Created by Sergej Grebenjuk on 08.10.14.
+ */
+public class AbstractFactoryTest {
+}
